df0c0a1eac5195e5cc90ed86cff403bc38d09d55
Andrey Bulanov <bulanov1804@gmail.com>
2021-07-24 20:38:30 +0300

    112

---
+++ src/refs.js
@@ -0,0 +1,7 @@
+// loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
+//     loadMoreLabel: document.querySelector(
+//         'button[data-action="load-more"] > .label',
+//     ),
+//     loadMoreSpinner: document.querySelector(
+//         'button[data-action="load-more"] > .spinner',
+//     ),
